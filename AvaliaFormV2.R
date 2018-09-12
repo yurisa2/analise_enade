a635fd9f304561d4bb5ec5e3b4a02803e0aff022
yurisa2 <yuri@sa2.com.br>
2018-09-12 01:03:14 -0300

    wok

--- AvaliaFormV2.R
+++ AvaliaFormV2.R
@@ -1,5 +1,8 @@
-PATH <- "C:/Bitnami/wampstack-7.1.20-1/apache2/htdocs/analise_enade"
-setwd(PATH)
+# PATH <- "C:/Bitnami/wampstack-7.1.20-1/apache2/htdocs/analise_enade"
+# setwd(PATH)
+
+# install.packages("FuzzyR")
+# install.packages("shiny")
 
 library(FuzzyR)
 
@@ -18,10 +21,10 @@ summary(teste)
 for(i in 1:ncol(teste)) {
 hist(as.numeric(teste[,i]),
      main={colnames(teste)[i]},
-     xlab="Passengers",
+     xlab="Respostas!",
      border="blue",
      col="green",
-     xlim=c(0,5),
+     xlim=c(1,5),
      prob = TRUE)
 }
 # Analise_Enade <- addRule(Analise_Enade, regras)
@@ -29,8 +32,13 @@ hist(as.numeric(teste[,i]),
 entrada <- matrix(c(teste[,5],teste[,6],teste[,7],teste[,8],teste[,9],teste[,10],teste[,4]), ncol = 7)
 entrada <- (entrada - 1) * 0.25
 
-TestarAluno_FIS <- readfis("C:/Bitnami/wampstack-7.1.20-1/apache2/htdocs/analise_enade/TestarAluno.fis")
-AjudarInstituicao_FIS <- readfis("C:/Bitnami/wampstack-7.1.20-1/apache2/htdocs/analise_enade/AjudarInstituicao.fis")
+TestarAluno_FIS <- readfis("TestarAluno.fis")
+AjudarInstituicao_FIS <- readfis("AjudarInstituicao.fis")
+
+# plotmf(TestarAluno_FIS, "output", 1)
+# showGUI(TestarAluno_FIS)
+
+
 
 results <- cbind (entrada,evalfis(entrada,TestarAluno_FIS))
 results <- cbind (results,evalfis(entrada,AjudarInstituicao_FIS))
