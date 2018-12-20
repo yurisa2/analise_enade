43dc5229ce9d6491ca8d997d95f70cb8ab163a7e
@yurisa2 MT5 <yuri@sa2.com.br>
2018-12-20 17:40:30 -0200

    v2

--- fuzzyV2.R
+++ fuzzyV2.R
@@ -7,32 +7,29 @@ r8b <- read.csv("resultsV2/8b.csv")
 r8g <- read.csv("resultsV2/8g.csv")
 
 
+
+labels <- read.csv("db/labels_v2.csv")
+
 total <- rbind(r7,r7b,r8b,r8g)
 
-summary(total)
+total[,1] <- NULL
 
-hist(total$X9..1)
+colnames(total) <- labels$label
 
-total_f <- NULL
 
 for(i in 1:ncol(total)) total[,i] <- as.numeric(total[,i])
 
+total_factor <- total
+for(i in 1:ncol(total_factor)) total_factor[,i] <- factor(total_factor[,i], exclude = NA)
 
+teste <- summary(total_factor)
 
-for(i in 2:ncol(total)) {
-  g <- total[,i]
-  m <- mean(g)
+write.csv(teste,file="teste.csv",row.names=TRUE,col.names=TRUE)
 
-  std<-sqrt(var(g))
+for(i in 1:ncol(total)) {
 
-  filename <- paste("plots/Segundo Enade ",i-1,".png")
+  filename <- paste("plots/Histogram Segundo Enade ",i,".png")
   png(filename, width = 640, height = 896)
-
-
-  hist(g,     main = paste("Histograma" , colnames(total)[i]), breaks = c(0,1,2,3,4,5), ylim = c(0,120))
-
-  curve(dnorm(x, mean=m, sd=std),
-        col="darkblue", lwd=2, add=TRUE, yaxt="n")
-
+  hist(total[,i],     main = paste("Histograma: ",colnames(total)[i]), breaks = 0:max(total[,i], na.rm=T), ylim = c(0,120))
   dev.off()
 }
